98daa9a8a0875ddcb6edd3a8d9f425716c002543
Matt Halverson <mhalverson@onekingslane.com>
2014-08-14 17:09:17 -0700

    Initialize empty repo

---
+++ test/danger_unicron/core_test.clj
@@ -0,0 +1,7 @@
+(ns danger-unicron.core-test
+  (:require [clojure.test :refer :all]
+            [danger-unicron.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
