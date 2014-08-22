2af97ab5f28385482a4a9581b9fb4fadf8428990
Matt Halverson <mhalverson@onekingslane.com>
2014-08-22 14:06:01 -0700

    More interpreter sketch

--- project.clj
+++ project.clj
@@ -4,4 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.onekingslane.danger/diesel "1.0.3"]])
+                 [org.clojure/tools.logging "0.2.6"]
+                 [com.onekingslane.danger/clojure-common-utils "0.0.24"]
+                 [com.onekingslane.danger/diesel "1.0.3"]
+                 [clj-time "0.7.0"]
+                 ;; [compojure "1.1.6"]
+                 ;; [ring "1.3.0"]
+                 ]
+  ;;:plugins [[lein-ring "0.8.11"]]
+  ;;:ring {:handler danger-unicron/handler}
+  :resource-paths ["resources"]
+)
--- src/danger_unicron/core.clj
+++ src/danger_unicron/core.clj
@@ -27,19 +27,17 @@
 
 (def- period-exprs
   ;; date functions pass out to clj-time library
-  {'years t/years
-   'months t/months
+  {'months t/months
    'weeks t/weeks
    'days t/days
    'hours t/hours
-   'minutes t/minutes
-   'seconds t/seconds})
+   'minutes t/minutes})
 
 (defn- period-expr? [expr] (contains? period-exprs (first expr)))
 (defn- poll-expr? [expr] (= 'poll-expr (first expr)))
 (defn- filter? [expr] (= 'filter (first expr)))
 
-(definterpreter interp-feed [env]
+(definterpreter interp-feed []
   ;; top-level
   ['feed => :feed]
   ;; date-expr for the feed
@@ -62,25 +60,28 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; # The implementation
 
-(defmethod interp-feed :feed [token date-expr action & optl]
+(defmethod interp-feed :feed [[token date-expr action & optl]]
   ;; optl may include one poll-expr and one filter
   (let [poll-exprs (filter poll-expr? optl)
         filters (filter filter? optl)]
     (when (> (count poll-exprs) 1)
-      (log-and-throw (format "Error: only allowed one poll-expr per feed.
-                              Several were specified: %s"
-                             poll-exprs)))
+      (log-and-throw (format
+                      (str "Error: only allowed one poll-expr per feed. "
+                           "Several were specified: %s")
+                      poll-exprs)))
     (when (> (count filters) 1)
-      (log-and-throw (format "Error: only allowed one filter per feed.
-                              Several were specified: %s"
-                             filters)))
+      (log-and-throw (format
+                      (str "Error: only allowed one filter per feed. "
+                           "Several were specified: %s")
+                      filters)))
+    (print-expr [token date-expr action optl])
     {:date-expr (interp-feed date-expr)
      :action (interp-feed action)
      :poll-expr (interp-feed (or (first poll-exprs)
                                  default-poll-expr))
      :filters (map interp-feed filters)}))
 
-(defmethod interp-feed :date-expr [token date-expr]
+(defmethod interp-feed :date-expr [[token date-expr]]
   ;; XXX
   ;; Validate that:
   ;;  - it contains at least one conversion spec with a defined granularity
@@ -89,7 +90,7 @@
   ;; Then just return the pattern-str.
   date-expr)
 
-(defmethod interp-feed :action [token typed-action]
+(defmethod interp-feed :action [[token typed-action]]
   ;; XXX
   ;; Returns a thunk that takes...
   ;;  - a filepath?
@@ -97,47 +98,49 @@
   ;; typed-action may be sh, root-sh, clj, ...
   (interp-feed typed-action))
 
-(defmethod interp-feed :sh [token cmd]
+(defmethod interp-feed :sh [[token cmd]]
   ;; Return a thunk that runs the specified shell command(s).
   ;; The thunk returns the map of exit code (:exit), std out (:out),
   ;; and std err (:err).
   #(sh "/bin/bash" "-c" cmd))
 
-(defmethod interp-feed :root-sh [token cmd]
+(defmethod interp-feed :root-sh [[token cmd]]
   (log-and-throw "root-sh has not been implemented"))
 
-(defmethod interp-feed :clj [token clj-code]
+(defmethod interp-feed :clj [[token clj-code]]
   ;; Return a thunk that runs the clj-code they specified.
   ;; The thunk returns whatever the overall body of code evaluates to.
   (cond (string? clj-code) #(eval (read-string clj-code))
         (list? clj-code)   #(eval clj-code)
-        (fn? clj-code)     #(apply clj-code '())))
+        (fn? clj-code)     #(apply clj-code '())
+        :else (log-and-throw "Don't know how to run your clj-type action")))
 
-(defmethod interp-feed :poll-expr [token typed-poll-expr]
+(defmethod interp-feed :poll-expr [[token typed-poll-expr]]
   ;; Returns EITHER a clj-time period, OR a list of cron-strs.
   (interp-feed typed-poll-expr))
 
-(defmethod interp-feed :interval [token period-expr]
+(defmethod interp-feed :interval [[token period-expr]]
   (interp-feed period-expr))
 
-(defmethod interp-feed :period-expr [period number]
+(defmethod interp-feed :period-expr [[period number]]
   (when (not (and (integer? number)
                   (pos? number)))
     (log-and-throw (format
-                    "A period-expr must contain a period and a positive
-                     integer. You didn't put a positive integer -- you put
-                     %s instead"
+                    (str
+                     "A period-expr must contain a period and a positive "
+                     "integer. You didn't put a positive integer -- you put "
+                     "%s instead")
                     number)))
   (let [period-maker (get period-exprs period)]
     (period-maker number)))
 
-(defmethod interp-feed :cron [token cron-str]
+(defmethod interp-feed :cron [[token cron-str]]
   ;; XXX
   ;; Validate that:
   ;;  - it's a valid cron
   (list cron-str))
 
-(defmethod interp-feed :crons [token & cron-strs]
+(defmethod interp-feed :crons [[token & cron-strs]]
   ;; XXX
   ;; Validate that:
   ;;  - they're all valid crons
@@ -146,10 +149,10 @@
     (log-and-throw "No crons specified in the \"crons\" block!"))
   cron-strs)
 
-(defmethod interp-feed :filter [token filter-expr]
+(defmethod interp-feed :filter [[token filter-expr]]
   ;; Returns a predicate that takes a filepath and indicates
   ;; whether or not it should actually be processed.
   (interp-feed filter-expr))
 
-(defmethod interp-feed :regex [token regex-pattern]
-  #(re-matches (re-pattern regex-pattern) %))
+(defmethod interp-feed :regex [[token regex-pattern]]
+  #(re-find (re-pattern regex-pattern) %))
