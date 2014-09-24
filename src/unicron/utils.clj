e2a656ed6b677dfa8994c297e81ac61d3cf321fc
Matt Halverson <mhalverson@onekingslane.com>
2014-09-24 07:59:43 -0700

    Flesh out utils.clj

--- src/unicron/utils.clj
+++ src/unicron/utils.clj
@@ -8,3 +8,15 @@
   `(do
      (log/error ~msg)
      (throw (RuntimeException. ~msg))))
+
+(defmacro assert-not-nil [sym]
+  `(when (nil? ~sym)
+     (log-and-throw (format "Error: %s was unspecified" ~(name sym)))))
+
+(defn arity
+  "Returns the arity of a function. Doesn't work for macros.
+See http://stackoverflow.com/questions/1696693/clojure-how-to-find-out-the-arity-of-function-at-runtime"
+  [f]
+  (let [m (first (.getDeclaredMethods (class f)))
+        p (.getParameterTypes m)]
+    (alength p)))
