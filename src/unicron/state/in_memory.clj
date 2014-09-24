29610547e49e337c894ced45b3bacab672900380
Matt Halverson <mhalverson@onekingslane.com>
2014-09-24 14:07:26 -0700

    First implementation of History -- in memory

---
+++ src/unicron/state/in_memory.clj
@@ -0,0 +1,111 @@
+(ns unicron.state.in-memory
+  "In-memory implementation of History"
+  {:author "Matt Halverson", :date "Mon Sep  8 16:43:36 PDT 2014"}
+  (:require [unicron.state :refer :all])
+  (:require [roxxi.utils.print :refer [print-expr]]
+            [clojure.tools.logging :as log]))
+
+;; # Helpers
+
+(defn- add-event [atom-vec event]
+  (swap! atom-vec conj event))
+
+(defn- generic-matches [events field val]
+  (filter (fn [e] (= val (field e))) events))
+(defn- date-expr-matches [events date-expr]
+  (generic-matches events :date-expr date-expr))
+
+(defn- sort-events-by-uri-time [event-seq]
+  (sort #(compare (:uri-time %1) (:uri-time %2))
+        event-seq))
+
+;; # The RAM implementation
+
+(defrecord InMemoryHistory [atom-vec]
+  History
+  ;; Read methods
+  (file-has-history? [_ date-expr]
+    (not (empty? (-> @atom-vec
+                     (generic-matches :date-expr date-expr)
+                     (generic-matches :is-dir false)))))
+  (dir-has-history? [_ date-expr]
+    (not (empty? (-> @atom-vec
+                     (generic-matches :date-expr date-expr)
+                     (generic-matches :is-dir true)))))
+  (latest-file-match [_ date-expr]
+    (-> @atom-vec
+        (generic-matches :date-expr date-expr)
+        (generic-matches :is-dir false)
+        (sort-events-by-uri-time)
+        (last)))
+  (latest-dir-match [_ date-expr]
+    (-> @atom-vec
+        (generic-matches :date-expr date-expr)
+        (generic-matches :is-dir true)
+        (sort-events-by-uri-time)
+        (last)))
+  (live-directories [_ date-expr]
+    (let [matching-dirs (-> @atom-vec
+                            (generic-matches :date-expr date-expr)
+                            (generic-matches :is-dir true))
+          dirs-with-starts (generic-matches matching-dirs :state :observed)
+          uris-with-starts (into #{} (map :uri dirs-with-starts))
+          dirs-with-ends (generic-matches matching-dirs :state :completed)
+          uris-with-ends (into #{} (map :uri dirs-with-ends))
+          live-uris (clojure.set/difference uris-with-starts uris-with-ends)
+          live-dirs (filter #(contains? live-uris (:uri %)) dirs-with-starts)]
+      ;; (print-expr uris-with-starts)
+      ;; (print-expr uris-with-ends)
+      ;; (print-expr live-dirs)
+      live-dirs))
+  (matches-in-directory [_ date-expr directory-uri]
+    (-> @atom-vec
+        (generic-matches :date-expr date-expr)
+        (generic-matches :is-dir false)
+        (generic-matches :dir-uri directory-uri)
+        (generic-matches :state :observed)))
+  ;; Write methods
+  (expire-old-directories! [h date-expr moment-of-death]
+    (let [to-be-reaped (filter #(< (:event-time %) moment-of-death)
+                               (live-directories h date-expr))]
+      (doseq [e to-be-reaped]
+        (log/infof "Expired old directory: uri was %s" (:uri e))
+        (completed-dir! h
+                        (now)
+                        (:date-expr e)
+                        (:uri e)
+                        (:uri-time e)
+                        (format "collected by the reaper; moment-of-death was %s" moment-of-death)))))
+  (observed-file! [_ observed-at date-expr uri uri-time msg]
+    (add-event atom-vec (make-file-event observed-at date-expr uri
+                                         uri-time :observed msg)))
+  (observed-dir! [_ observed-at date-expr uri uri-time msg]
+    (add-event atom-vec (make-dir-event observed-at date-expr uri
+                                        uri-time :observed msg)))
+  (observed-file-in-dir! [_ observed-at date-expr uri uri-time msg dir-uri]
+    (add-event atom-vec (make-file-in-dir-event observed-at date-expr uri
+                                                uri-time :observed msg
+                                                dir-uri)))
+  (processing-file! [_ started-at date-expr uri uri-time msg]
+    (add-event atom-vec (make-file-event started-at date-expr uri
+                                         uri-time :processing msg)))
+  (processing-dir! [_ started-at date-expr uri uri-time msg]
+    (add-event atom-vec (make-dir-event started-at date-expr uri
+                                        uri-time :processing msg)))
+  (processing-file-in-dir! [_ started-at date-expr uri uri-time msg dir-uri]
+    (add-event atom-vec (make-file-in-dir-event started-at date-expr uri
+                                                uri-time :processing msg
+                                                dir-uri)))
+  (completed-file! [_ completed-at date-expr uri uri-time msg]
+    (add-event atom-vec (make-file-event completed-at date-expr uri
+                                         uri-time :completed msg)))
+  (completed-dir! [_ completed-at date-expr uri uri-time msg]
+    (add-event atom-vec (make-dir-event completed-at date-expr uri
+                                        uri-time :completed msg)))
+  (completed-file-in-dir! [_ completed-at date-expr uri uri-time msg dir-uri]
+    (add-event atom-vec (make-file-in-dir-event completed-at date-expr uri
+                                                uri-time :completed msg
+                                                dir-uri))))
+
+(defn make-in-memory-history []
+  (->InMemoryHistory (atom [])))
---
+++ test/unicron/state_test.clj
@@ -0,0 +1,116 @@
+(ns unicron.state-test
+  {:author "Matt Halverson", :date "Wed Sep 24 09:52:10 PDT 2014"}
+  (:require [clojure.test :refer :all]
+            [unicron.state :refer :all]
+            [unicron.state.in-memory :refer [make-in-memory-history]]
+            [unicron.state.sqlite :refer [make-sqlite-history]]
+            [clj-time.core :refer [seconds minutes hours days weeks]]))
+
+;; # State tests
+
+(def sep-24-2014 1411577810)
+
+(defn seconds-ago [sec] (- sep-24-2014 sec))
+(defn minutes-ago [min] (seconds-ago (* 60 min)))
+(defn hours-ago [hrs] (minutes-ago (* 60 hrs)))
+(defn days-ago [days] (hours-ago (* 24 days)))
+(defn weeks-ago [weeks] (days-ago (* 7 weeks)))
+
+(deftest moment-of-death-test
+  (with-redefs
+    [unicron.state/now (constantly sep-24-2014)]
+    (testing "Moments of death are correctly computed for"
+      (testing "ints"
+        (is (= (seconds-ago 5) (moment-of-death 5)))
+        (is (= (seconds-ago 532) (moment-of-death 532))))
+      (testing "joda Periods"
+        (is (= (seconds-ago 5) (moment-of-death (seconds 5))))
+        (is (= (minutes-ago 4) (moment-of-death (minutes 4))))
+        (is (= (hours-ago 3)   (moment-of-death (hours 3))))
+        (is (= (days-ago 2)    (moment-of-death (days 2))))
+        (is (= (weeks-ago 1)   (moment-of-death (weeks 1))))))
+    (testing "Moments of death can't be computed for other types"
+      (is (thrown? Exception (moment-of-death "asdf")))
+      (is (thrown? Exception (moment-of-death :a)))
+      (is (thrown? Exception (moment-of-death {})))
+      (is (thrown? Exception (moment-of-death [])))
+      (is (thrown? Exception (moment-of-death '()))))))
+
+;; # In-memory tests
+
+(defn now-ms [] (System/currentTimeMillis))
+(def de-1 "s3://foo/bar/%Y/")
+(def de-2 "s3://foo/baz/%Y/")
+
+
+(defmacro expand-into-state-tests [h-type h]
+  (let [test-name (symbol (str h-type "-test"))]
+    `(deftest ~test-name
+       (testing "observing files"
+         (let [h# ~h]
+           (is (false? (file-has-history? h# de-1)))
+           (is (false? (file-has-history? h# de-2)))
+           (is (nil? (latest-file-match h# de-1)))
+           (is (nil? (latest-file-match h# de-2)))
+           (observed-file! h# (now-ms) de-1 "s3://foo/bar/2014/" 44000
+                           "saw one")
+
+           (is (true? (file-has-history? h# de-1)))
+           (is (false? (file-has-history? h# de-2)))
+           (is (= "s3://foo/bar/2014/" (:uri (latest-file-match h# de-1))))
+           (is (nil? (latest-file-match h# de-2)))
+
+           (observed-file! h# (now-ms) de-1 "s3://foo/bar/2015/" 44001
+                           "saw another one")
+
+           (is (= "s3://foo/bar/2015/" (:uri (latest-file-match h# de-1))))))
+
+       (testing "observing directories"
+         (let [h# ~h]
+           (is (false? (dir-has-history? h# de-1)))
+           (observed-dir! h# 1000 de-1 "s3://foo/bar/2014/" 44000
+                          "saw one; continuing to monitor for 24 hours from now")
+           (is (true? (dir-has-history? h# de-1)))
+           (is (= 0 (count (matches-in-directory h# de-1 "s3://foo/bar/2014/"))))
+           (observed-file-in-dir! h# 1000 de-1 "s3://foo/bar/2014/a.txt" 44000
+                                  "found a file in the dir" "s3://foo/bar/2014/")
+           (is (= 1 (count (matches-in-directory h# de-1 "s3://foo/bar/2014/"))))
+           (observed-file-in-dir! h# 1002 de-1 "s3://foo/bar/2014/b.txt" 44001
+                                  "found a file in the dir" "s3://foo/bar/2014/")
+           (is (= 2 (count (matches-in-directory h# de-1 "s3://foo/bar/2014/"))))
+           (observed-file-in-dir! h# 1003 de-1 "s3://foo/bar/2014/c.txt" 44002
+                                  "found a file in the dir" "s3://foo/bar/2014/")
+           (is (= 3 (count (matches-in-directory h# de-1 "s3://foo/bar/2014/"))))
+           (is (true? (dir-has-history? h# de-1)))
+           (is (= "s3://foo/bar/2014/" (:uri (latest-dir-match h# de-1))))
+           (is (= 1 (count (live-directories h# de-1))))
+
+           (observed-dir! h# 2000 de-1 "s3://foo/bar/2015/" 45000
+                          "saw another, monitoring for a while")
+           (is (true? (dir-has-history? h# de-1)))
+           (is (= 2 (count (live-directories h# de-1))))
+           (is (= "s3://foo/bar/2015/" (:uri (latest-dir-match h# de-1))))
+
+           (expire-old-directories! h# de-1 1999)
+           (is (= 1 (count (live-directories h# de-1))))
+           (is (= 3 (count (matches-in-directory h# de-1 "s3://foo/bar/2014/"))))
+           (is (= 0 (count (matches-in-directory h# de-1 "s3://foo/bar/2015/"))))
+           (is (= "s3://foo/bar/2015/" (:uri (latest-dir-match h# de-1))))
+
+           (expire-old-directories! h# de-1 2001)
+           (is (= 0 (count (live-directories h# de-1))))
+           (is (= 3 (count (matches-in-directory h# de-1 "s3://foo/bar/2014/"))))
+           (is (= 0 (count (matches-in-directory h# de-1 "s3://foo/bar/2015/"))))
+           (is (= "s3://foo/bar/2015/" (:uri (latest-dir-match h# de-1))))
+
+           (is (false? (dir-has-history? h# de-2)))
+           (is (= 0 (count (live-directories h# de-2)))))))))
+
+(expand-into-state-tests "in-memory-history" (make-in-memory-history))
+;; XXX uncomment this when sqlite-history is implemented
+;; (expand-into-state-tests "sqlite-history" (make-sqlite-history))
+
+
+;; XX Flesh this out with generative testing :D
+;; https://github.com/clojure/test.check
+;; https://github.com/clojure/test.generative
